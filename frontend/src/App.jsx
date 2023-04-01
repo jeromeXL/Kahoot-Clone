2aa33ffa38208e6f9dbc59a5f8de6146b07343c6
Harrison Chow <harrychow@Harrisons-MacBook-Pro.local>
2023-04-01 11:35:11 +1100

    playing around with routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
+
+import Login from './components/Login.js';
+import Signup from './components/Signup.js';
+
+function Hello () {
+  return (<div>Hello</div>);
+}
 
 function App () {
   return (
-    <>Let&apos;s go!</>
+    <>
+      <>Let&apos;s go!</>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Hello/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/signup" element={<Signup/>} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
