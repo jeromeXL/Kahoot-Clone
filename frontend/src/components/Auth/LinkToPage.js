c970159701c6d17be0c8cdf1ebfec2ac9a5b61f2
Harrison Chow <harrychow@Harrisons-MacBook-Pro.local>
2023-04-01 18:20:38 +1100

    control font size in link to page

--- frontend/src/components/Auth/LinkToPage.js
+++ frontend/src/components/Auth/LinkToPage.js
@@ -2,5 +2,5 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function LinkToPage (props) {
-  return (<div style={{ color: 'white', padding: '5px' }}>{props.text} <Link to={props.page}> {props.linkText} </Link></div>);
+  return (<div style={{ color: 'white', padding: '10px', fontSize: props.fontSize ? `${props.fontSize}` : '12pt' }}>{props.text} <Link to={props.page}> {props.linkText} </Link></div>);
 }
