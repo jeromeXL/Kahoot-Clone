f7d0e52e9c0d619d3b8afc813f1e7265c1f823a6
Harrison Chow <harrychow@Harrisons-MacBook-Pro.local>
2023-04-02 11:45:37 +1000

    minor changes to default page

--- frontend/src/components/DefaultScreen/Default.js
+++ frontend/src/components/DefaultScreen/Default.js
@@ -2,25 +2,20 @@ import React from 'react';
 import Centre from '../General/Centre';
 import Title from '../General/Title';
 import LinkToPage from '../General/LinkToPage';
-import SubmitButton from '../General/SubmitButton';
 import Subtitle from '../General/Subtitle';
 import FormContainer from '../General/FormContainer'
 
 export default function Default () {
   return (
     <>
-      <Centre color="#AAB8D4">
+      <Centre color="#39548D">
         <Title/>
-        <FormContainer>
-          <SubmitButton color="#475A81">
-            <LinkToPage text="" page="./login" linkText="Click here to be an Admin" color="white" underline="false"/>
-          </SubmitButton>
+        <FormContainer color="#6178A8">
+          <LinkToPage text="" page="./login" linkText="Click here to be an Admin" color="white" fontSize='15pt'/>
           <Subtitle>
             Or
           </Subtitle>
-          <SubmitButton color="#005050">
-            <LinkToPage text="" page="./join" linkText="Click here to be a Player" color="white" underline="false"/>
-          </SubmitButton>
+          <LinkToPage text="" page="./join" linkText="Click here to be a Player" color="white" fontSize='15pt'/>
         </FormContainer>
       </Centre>
     </>
--- frontend/src/components/General/LinkToPage.js
+++ frontend/src/components/General/LinkToPage.js
@@ -2,5 +2,5 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function LinkToPage (props) {
-  return (<div style={{ color: 'white', padding: '10px', fontSize: props.fontSize ? `${props.fontSize}` : '12pt' }}>{props.text} <Link to={props.page} style={{ color: props.color, textDecoration: props.underline ? 'none' : 'default' }}> {props.linkText} </Link></div>);
+  return (<div style={{ color: 'white', padding: '10px', fontSize: props.fontSize ? props.fontSize : '12pt', textAlign: 'center' }}>{props.text} <Link to={props.page} style={{ color: props.color }}> {props.linkText} </Link></div>);
 }
