44f80b602092f76f57b4cfdbe74790c8aee5fd93
Harrison Chow <harrychow@Harrisons-MacBook-Pro.local>
2023-04-05 17:01:14 +1000

    navigation and passing through token

--- frontend/src/components/Dashboard/Dashboard.js
+++ frontend/src/components/Dashboard/Dashboard.js
@@ -3,10 +3,36 @@ import DashboardHeader from './DashboardHeader';
 import '@fontsource/inter';
 import GamesContainer from './GamesContainer';
 import GameInDashBoard from './GameInDashboard';
-// import { token as loginToken} from '../Auth/Login';
-// import Container from 'react-bootstrap/Container';
+import { useLocation } from 'react-router-dom';
+import data from '../../config.json';
+
+let token;
+const BACKEND_PORT = data.BACKEND_PORT;
+const url = `http://localhost:${BACKEND_PORT}`;
 
 export default function Dashboard () {
+  const location = useLocation();
+  token = location.state.token;
+  console.log(token);
+  fetch(url + '/admin/quiz', {
+    method: 'GET',
+    headers: { accept: 'application/json', Authorization: `Bearer ${token}` },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        console.log('Response error when loading game feed');
+        throw new Error('Invalid Token/Access');
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log('games data ', data);
+    })
+    .catch((err) => {
+      console.log('Other error when loading job feed');
+      console.log(err);
+    });
+
   return (
     <>
       <DashboardHeader/>
